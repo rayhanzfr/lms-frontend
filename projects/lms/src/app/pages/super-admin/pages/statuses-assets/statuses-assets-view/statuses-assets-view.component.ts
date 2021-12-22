da810f911f820aed848f2079214a02e49ce37cab
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-22 15:55:07 +0700

    create component module routing statuses assets

---
+++ projects/lms/src/app/pages/super-admin/pages/statuses-assets/statuses-assets-view/statuses-assets-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statuses-assets-view',
+  templateUrl: './statuses-assets-view.component.html',
+  styleUrls: ['./statuses-assets-view.component.css']
+})
+export class StatusesAssetsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
