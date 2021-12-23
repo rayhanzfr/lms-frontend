bf1f8ba8497b40e555d981601898ba4a64213885
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-23 17:39:48 +0700

    add http service companies, invoices, locations, statuses assets

--- projects/core/src/app/services/invoices/invoices.service.ts
+++ projects/core/src/app/services/invoices/invoices.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GetAllInvoicesResDto } from '../../dto/invoices/get-all-invoices-res-dto';
+import { GetByIdInvoicesResDto } from '../../dto/invoices/get-by-id-invoices-res-dto';
+import { Invoices } from '../../dto/invoices/invoices';
+import { GetByCodeLocationsResDto } from '../../dto/locations/get-by-code-locations-res-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvoicesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<GetAllInvoicesResDto>{
+    return this.http.get<GetAllInvoicesResDto>('http://localhost:8888/invoices/')
+  }
+
+  getById(): Observable<GetByIdInvoicesResDto>{
+    return this.http.get<GetByIdInvoicesResDto>('http://localhost:8888/invoices/id')
+  }
+
+  getByCode(): Observable<GetByCodeLocationsResDto>{
+    return this.http.get<GetByCodeLocationsResDto>('http://localhost:8888/invoices/code')
+  }
+  
+  save(invoices: Invoices): Observable<Invoices>{
+    return this.http.post<Invoices>('http://localhost:8888/invoices/', invoices)
+  }
+
+  update(invoices: Invoices): Observable<Invoices>{
+    return this.http.put<Invoices>('http://localhost:8888/invoices/', invoices)
+  }
+
+  delete(id: string): Observable<Invoices>{
+    return this.http.delete<Invoices>('http://localhost:8888/invoices/' + id)
+  }
 }
--- projects/core/src/app/services/locations/locations.service.ts
+++ projects/core/src/app/services/locations/locations.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GetAllLocationsResDto } from '../../dto/locations/get-all-locations-res-dto';
+import { GetByCodeLocationsResDto } from '../../dto/locations/get-by-code-locations-res-dto';
+import { GetByIdLocationsResDto } from '../../dto/locations/get-by-id-locations-res-dto';
+import { Locations } from '../../dto/locations/locations';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<GetAllLocationsResDto> {
+    return this.http.get<GetAllLocationsResDto>('http://localhost:8888/locations/');
+  }
+
+  getById(): Observable<GetByIdLocationsResDto> {
+    return this.http.get<GetByIdLocationsResDto>('http://localhost:8888/locations/id');
+  }
+
+  getByCode(): Observable<GetByCodeLocationsResDto> {
+    return this.http.get<GetByCodeLocationsResDto>('http://localhost:8888/locations/code');
+  }
+
+  save(locations: Locations): Observable<Locations> {
+    return this.http.post<Locations>('http://localhost:8888/locations/', locations);
+  }
+
+  update(locations: Locations): Observable<Locations> {
+    return this.http.put<Locations>('http://localhost:8888/locations/', locations);
+  }
+
+  delete(id: string): Observable<Locations> {
+    return this.http.delete<Locations>('http://localhost:8888/locations/' + id);
+  }
+
 }
