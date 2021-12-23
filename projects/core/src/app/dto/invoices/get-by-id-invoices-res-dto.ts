e2c8a70c34b6c0e2c69568ecbc13edf02e4d5723
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-23 17:35:38 +0700

    add dto invoices

---
+++ projects/core/src/app/dto/invoices/get-by-id-invoices-res-dto.ts
@@ -0,0 +1,6 @@
+import { Invoices } from "./invoices"
+
+export class GetByIdInvoicesResDto {
+	invoices!: Invoices
+	message!: string
+}
---
+++ projects/core/src/app/dto/invoices/invoices.ts
@@ -0,0 +1,14 @@
+export class Invoices {
+
+	id!: string
+	version!: number
+	createdAt!: string
+	createdBy!: string
+	updatedAt!: string
+	updatedBy!: string
+	isActive!: boolean
+	invoicesCode!: string
+	invoicesDate!: string
+	storeName!: string
+	price!: number
+}
