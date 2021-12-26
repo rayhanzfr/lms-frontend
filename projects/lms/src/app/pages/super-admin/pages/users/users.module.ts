931dc38ae4d8a5223c2dd350f5d755446d2cbd24
BondhanJantriR <67788316+BondhanJantriR@users.noreply.github.com>
2021-12-26 13:38:43 +0700

    update

--- projects/lms/src/app/pages/super-admin/pages/users/users.module.ts
+++ projects/lms/src/app/pages/super-admin/pages/users/users.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UsersComponent } from './users.component';
 import { UsersModifyComponent } from './users-modify/users-modify.component';
 import { UsersViewComponent } from './users-view/users-view.component';
+import { UsersRoutingModule } from './users-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    UsersComponent,
     UsersModifyComponent,
     UsersViewComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, UsersRoutingModule
   ]
 })
 export class UsersModule { }
