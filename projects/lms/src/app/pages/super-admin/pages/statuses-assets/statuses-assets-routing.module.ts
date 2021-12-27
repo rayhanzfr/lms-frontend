46372ed057d16369af1f16d981d3227f62356eca
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-27 10:19:19 +0700

    add master statuses assets

--- projects/lms/src/app/pages/super-admin/pages/statuses-assets/statuses-assets-routing.module.ts
+++ projects/lms/src/app/pages/super-admin/pages/statuses-assets/statuses-assets-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { StatusesAssetsViewComponent } from './statuses-assets-view/statuses-assets-view.component';
 
-
+const routes: Routes = [
+  {
+    path: 'admin/statuses-assets',
+    component:StatusesAssetsViewComponent
+  }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(routes)
   ]
 })
 export class StatusesAssetsRoutingModule { }
--- projects/lms/src/app/pages/super-admin/pages/statuses-assets/statuses-assets-view/statuses-assets-view.component.ts
+++ projects/lms/src/app/pages/super-admin/pages/statuses-assets/statuses-assets-view/statuses-assets-view.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { GetAllStatusesAssetsResDto } from 'projects/core/src/app/dto/statuses-assets/get-all-statuses-assets-res-dto';
+import { StatusesAssetsService } from 'projects/core/src/app/services/statuses-assets/statuses-assets.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-statuses-assets-view',
   templateUrl: './statuses-assets-view.component.html',
   styleUrls: ['./statuses-assets-view.component.css']
 })
-export class StatusesAssetsViewComponent implements OnInit {
+export class StatusesAssetsViewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  obs?: Subscription
+  data?: GetAllStatusesAssetsResDto
+
+  constructor(private statusesAssetsService: StatusesAssetsService) { }
+  ngOnDestroy(): void {
+    // this.obs?.unsubscribe;
+  }
 
   ngOnInit(): void {
+    this.data = new GetAllStatusesAssetsResDto
+    // this.statusesAssetsService.getAll().subscribe(result => this.data = result)
   }
 
 }
