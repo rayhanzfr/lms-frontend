9822902b0d024a8cebca2e867668cbb3eede3ddf
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-27 20:55:23 +0700

    add Files model

---
+++ projects/core/src/app/dto/files/files.ts
@@ -0,0 +1,11 @@
+export class Files{
+    id!:string;
+    version!:number;
+    createdBy!: string;
+    createdAt!: string
+    updatedAt!: string
+    updatedBy!: string
+    isActive!:boolean
+    file!: string
+    extensions!: string
+}
