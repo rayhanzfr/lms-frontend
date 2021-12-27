c8fb4ad3b51496ee8ab49b6f0445c79d8df8db3a
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-27 20:59:15 +0700

    add view

--- projects/lms/src/app/pages/super-admin/pages/locations/locations-view/locations-view.component.ts
+++ projects/lms/src/app/pages/super-admin/pages/locations/locations-view/locations-view.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Locations } from 'projects/core/src/app/dto/locations/locations';
+import { LocationsService } from 'projects/core/src/app/services/locations/locations.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-locations-view',
   templateUrl: './locations-view.component.html',
   styleUrls: ['./locations-view.component.css']
 })
-export class LocationsViewComponent implements OnInit {
+export class LocationsViewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  obs?: Subscription
+  data: Locations[] = []
+
+  constructor(private locationsService: LocationsService) { }
+  ngOnDestroy(): void {
+    this.obs?.unsubscribe
+  }
 
   ngOnInit(): void {
+    this.locationsService.getAll().subscribe(result => this.data = result)
   }
 
 }
