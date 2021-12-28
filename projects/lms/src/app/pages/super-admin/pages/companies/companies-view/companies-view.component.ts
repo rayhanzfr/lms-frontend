3108029de5f91732d2bdec8b81c4d398b95db40c
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-28 15:20:15 +0700

    fix companies

--- projects/lms/src/app/pages/super-admin/pages/companies/companies-view/companies-view.component.ts
+++ projects/lms/src/app/pages/super-admin/pages/companies/companies-view/companies-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Companies } from 'projects/core/src/app/dto/companies/companies';
+import { DeleteCompaniesResDto } from 'projects/core/src/app/dto/companies/delete-companies-res-dto';
 import { CompaniesService } from 'projects/core/src/app/services/companies/companies.service';
 import { Subscription } from 'rxjs';
 
@@ -12,8 +14,9 @@ export class CompaniesViewComponent implements OnInit, OnDestroy {
 
   obs?: Subscription
   data: Companies[] = []
+  resDeleteCompanies?:DeleteCompaniesResDto
 
-  constructor(private companiesService: CompaniesService) { }
+  constructor(private router:Router, private companiesService: CompaniesService) { }
   ngOnDestroy(): void {
     this.obs?.unsubscribe
   }
@@ -22,4 +25,20 @@ export class CompaniesViewComponent implements OnInit, OnDestroy {
     this.companiesService.getAll().subscribe(result => this.data = result)
   }
 
+  gotoInsert(){
+    this.router.navigateByUrl('admin/companies/new')
+  }
+
+  gotoUpdate(code:string):void{
+    this.router.navigateByUrl(`admin/companies/${code}`)
+  }
+
+  delete(id:string){
+    if(confirm("Are you sure?")){
+      this.companiesService.delete(id).subscribe(result=>{
+        this.resDeleteCompanies=result
+        window.location.reload()
+      })
+    }
+  }
 }
