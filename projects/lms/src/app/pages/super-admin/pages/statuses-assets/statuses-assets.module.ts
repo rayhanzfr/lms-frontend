ac715de6a6328a5c724b7f091c3e55136613a85e
Bahrul Faizi <bahrul.faizi@gmail.com>
2021-12-28 14:03:53 +0700

    fix stat asset

--- projects/lms/src/app/pages/super-admin/pages/statuses-assets/statuses-assets.module.ts
+++ projects/lms/src/app/pages/super-admin/pages/statuses-assets/statuses-assets.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatusesAssetsViewComponent } from './statuses-assets-view/statuses-assets-view.component';
-import { StatusesAssetsModifyComponent } from './statuses-assets-modify/statuses-assets-modify.component';
 import { StatusesAssetsRoutingModule } from './statuses-assets-routing.module';
 import { TableModule } from 'primeng/table';
 import {CheckboxModule} from 'primeng/checkbox';
@@ -9,8 +8,7 @@ import {CheckboxModule} from 'primeng/checkbox';
 
 @NgModule({
   declarations: [
-    StatusesAssetsViewComponent,
-    StatusesAssetsModifyComponent
+    StatusesAssetsViewComponent
   ],
   imports: [
     CommonModule, StatusesAssetsRoutingModule, TableModule, CheckboxModule
