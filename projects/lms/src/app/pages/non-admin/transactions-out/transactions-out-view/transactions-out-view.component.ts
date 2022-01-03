06d2f6db71c1a8efcbf3f6efa6df7bd909fce5fa
rayhanzfr <rayhanzaf@gmail.com>
2022-01-03 10:06:04 +0700

    update transactions in

--- projects/lms/src/app/pages/non-admin/transactions-out/transactions-out-view/transactions-out-view.component.ts
+++ projects/lms/src/app/pages/non-admin/transactions-out/transactions-out-view/transactions-out-view.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GetAllTransactionsOutByUsersResDto } from 'projects/core/src/app/dto/transactions-out/get-all-transactions-out-by-users-res-dto';
+import { TransactionsOutService } from 'projects/core/src/app/services/transactions-out/transactions-out.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-transactions-out-view',
   templateUrl: './transactions-out-view.component.html',
   styleUrls: ['./transactions-out-view.component.css']
 })
-export class TransactionsOutViewComponent implements OnInit {
+export class TransactionsOutViewComponent implements OnInit,OnDestroy{
 
-  constructor() { }
+  constructor(private router:Router,private transactionsOutService:TransactionsOutService) { }
+  data:GetAllTransactionsOutByUsersResDto=new GetAllTransactionsOutByUsersResDto();
+  obs?:Subscription
 
+  ngOnDestroy():void{
+    this.obs?.unsubscribe();
+  }
   ngOnInit(): void {
+    this.obs=this.transactionsOutService.getAllByUsers().subscribe(result=>{
+      this.data=result
+    })
   }
 
 }
