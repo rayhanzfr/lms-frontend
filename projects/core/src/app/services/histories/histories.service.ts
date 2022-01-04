b377eb7baac97ea05492e953d7eb2011b1aed0a2
rayhanzfr <rayhanzaf@gmail.com>
2022-01-04 11:17:04 +0700

    add generate pdf histories

--- projects/core/src/app/services/histories/histories.service.ts
+++ projects/core/src/app/services/histories/histories.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GetAllHistoriesResDto } from '../../dto/histories/get-all-histories-res-dto';
+import { HistoriesReportResDto } from '../../dto/histories/histories-report-res-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,8 @@ export class HistoriesService {
   constructor(private http: HttpClient) { }
   getAll(): Observable<GetAllHistoriesResDto>{
     return this.http.get<GetAllHistoriesResDto>('http://localhost:8888/histories/')
+  }
+  generatePdf():Observable<HistoriesReportResDto[]>{
+    return this.http.get<HistoriesReportResDto[]>(`http://localhost:8888/histories/pdf`)
   }
 }
